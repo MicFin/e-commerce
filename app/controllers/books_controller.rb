6fda507f3dc0e703275ccf5a75e2dd77799142ff
Michael Finneran <mrfinneran@gmail.com>
2016-01-31 19:34:33 -0500

    Added book user association and created dashboard

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,13 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
-
+  # Ensure the user is logged in.
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+  
+  
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.where(availability: true)
   end
 
   # GET /books/1
@@ -24,7 +27,7 @@ class BooksController < ApplicationController
   # POST /books
   # POST /books.json
   def create
-    @book = Book.new(book_params)
+    @book = current_user.books.new(book_params)
 
     respond_to do |format|
       if @book.save
@@ -69,6 +72,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:name, :author, :description)
+      params.require(:book).permit(:name, :description, :price, :availability, :author)
     end
 end
